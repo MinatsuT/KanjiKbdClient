0f23ace495b3a01f140ea91ab2a43b402de24bc0
Minatsu Tukisima <tksm372@gmail.com>
2018-03-13 04:34:14 +0900

    Initial version

---
+++ KanjiKbd/KeyCode.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace KanjiKbd {
+    class KeyCode {
+        public static readonly byte MOD_LCTRL = 1<<0;
+        public static readonly byte MOD_LSHIFT = 1<<1;
+        public static readonly byte MOD_LALT = 1<<2;
+        public static readonly byte MOD_LWINDOWS = 1<<3;
+        public static readonly byte MOD_RCTRL = 1 << 4;
+        public static readonly byte MOD_RSHIFT = 1 << 5;
+        public static readonly byte MOD_RALT = 1 << 6;
+        public static readonly byte MOD_RWINDOWS = 1 << 7;
+
+        public static readonly int[] asciiToScanCode = {
+            //  0      1      2      3      4      5      6      7      8      9      A      B      C      D      E      F
+            0x02C, 0x11E, 0x11F, 0x120, 0x121, 0x122, 0x123, 0x124, 0x125, 0x126, 0x134, 0x133, 0x036, 0x02d, 0x037, 0x038, //0x20-0x2F
+            0x027, 0x01E, 0x01F, 0x020, 0x021, 0x022, 0x023, 0x024, 0x025, 0x026, 0x034, 0x033, 0x136, 0x12d, 0x137, 0x138, //0x30-0x3F
+            0x02f, 0x104, 0x105, 0x106, 0x107, 0x108, 0x109, 0x10A, 0x10B, 0x10C, 0x10D, 0x10E, 0x10F, 0x110, 0x111, 0x112, //0x40-0x4F
+            0x113, 0x114, 0x115, 0x116, 0x117, 0x118, 0x119, 0x11A, 0x11B, 0x11C, 0x11D, 0x030, 0x089, 0x032, 0x02e, 0x187, //0x50-0x5F
+            0x12f, 0x004, 0x005, 0x006, 0x007, 0x008, 0x009, 0x00A, 0x00B, 0x00C, 0x00D, 0x00E, 0x00F, 0x010, 0x011, 0x012, //0x60-0x6F
+            0x013, 0x014, 0x015, 0x016, 0x017, 0x018, 0x019, 0x01A, 0x01B, 0x01C, 0x01D, 0x130, 0x189, 0x132, 0x12e};       //0x70-0x7E
+
+        public static readonly byte[] keyToScanCode = new byte[256];
+
+        public static byte getCode(Key k) {
+            return keyToScanCode[(int)k];
+        }
+
+        static KeyCode() {
+            // A(0x04)-Z(0x1D)
+            for (int i = (int)Key.A; i <= (int)Key.Z; i++) {
+                keyToScanCode[i] = (byte)(0x04 + (i - (int)Key.A));
+            }
+
+            // 1(0x1E)-9(0x26)
+            for (int i = (int)Key.D1; i <= (int)Key.D9; i++) {
+                keyToScanCode[i] = (byte)(0x1E + (i - (int)Key.D1));
+            }
+
+            // 0(0x27)
+            keyToScanCode[(int)Key.D0] = 0x27;
+
+            keyToScanCode[(int)Key.Enter] = 0x28;
+            keyToScanCode[(int)Key.Escape] = 0x29;
+            keyToScanCode[(int)Key.Back] = 0x2A;
+            keyToScanCode[(int)Key.Tab] = 0x2B;
+            keyToScanCode[(int)Key.Space] = 0x2C;
+            keyToScanCode[(int)Key.OemMinus] = 0x2D; // -
+            keyToScanCode[(int)Key.OemQuotes] = 0x2E; // ^
+            keyToScanCode[(int)Key.Oem5] = 0x89; // \
+            keyToScanCode[(int)Key.Oem3] = 0x2f; // @
+            keyToScanCode[(int)Key.OemOpenBrackets] = 0x30; // [
+            keyToScanCode[(int)Key.OemPlus] = 0x33; // ;
+            keyToScanCode[(int)Key.Oem1] = 0x34; // :
+            keyToScanCode[(int)Key.Oem6] = 0x32; // ]
+            keyToScanCode[(int)Key.OemComma] = 0x36; // ,
+            keyToScanCode[(int)Key.OemPeriod] = 0x37; // .
+            keyToScanCode[(int)Key.OemQuestion] = 0x38; // /
+            keyToScanCode[(int)Key.OemBackslash] = 0x87; // _
+
+            // F1(0x3A)-F12(0x45)
+            for (int i = (int)Key.F1; i <= (int)Key.F12; i++) {
+                keyToScanCode[i] = (byte)(0x3A + (i - (int)Key.F1));
+            }
+
+            keyToScanCode[(int)Key.PrintScreen] = 0x46;
+            keyToScanCode[(int)Key.Scroll] = 0x47;
+            keyToScanCode[(int)Key.Pause] = 0x48;
+
+            keyToScanCode[(int)Key.Insert] = 0x49;
+            keyToScanCode[(int)Key.Home] = 0x4A;
+            keyToScanCode[(int)Key.PageUp] = 0x4B;
+            keyToScanCode[(int)Key.Delete] = 0x4C;
+            keyToScanCode[(int)Key.End] = 0x4D;
+            keyToScanCode[(int)Key.PageDown] = 0x4E;
+
+            keyToScanCode[(int)Key.Right] = 0x4F;
+            keyToScanCode[(int)Key.Left] = 0x50;
+            keyToScanCode[(int)Key.Down] = 0x51;
+            keyToScanCode[(int)Key.Up] = 0x52;
+
+            keyToScanCode[(int)Key.NumLock] = 0x53;
+            keyToScanCode[(int)Key.Divide] = 0x54;
+            keyToScanCode[(int)Key.Multiply] = 0x55;
+            keyToScanCode[(int)Key.Subtract] = 0x56;
+            keyToScanCode[(int)Key.Add] = 0x57;
+            keyToScanCode[(int)Key.Return] = 0x58;
+
+            // Num1(0x59)-Num9(0x61)
+            for (int i = (int)Key.NumPad1; i <= (int)Key.NumPad9; i++) {
+                keyToScanCode[i] = (byte)(0x59 + (i - (int)Key.NumPad1));
+            }
+
+            // Num0(0x62)
+            keyToScanCode[(int)Key.NumPad0] = 0x62;
+
+        }
+    }
+}
---
+++ KanjiKbd/MainWindow.xaml.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KanjiKbd {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+        private SerialPort myPort;
+        private byte[] buf = new byte[1024];
+        private readonly byte XOFF = 0x13; // Ctrl+S
+        private readonly byte XON = 0x11; // Ctrl+Q
+        private Boolean flowStop = false;
+
+        /// <summary>
+        /// IMEで変換中かのフラグ
+        /// </summary>
+        private bool _imeFlag = false;
+
+        public MainWindow() {
+            InitializeComponent();
+
+            TextCompositionManager.AddPreviewTextInputHandler(TB, OnPreviewTextInput);
+            TextCompositionManager.AddPreviewTextInputStartHandler(TB, OnPreviewTextInputStart);
+            TextCompositionManager.AddPreviewTextInputUpdateHandler(TB, OnPreviewTextInputUpdate);
+
+            // COMポートを列挙し、最後に見つかったポートをオープンする
+            string portName = null;
+            foreach (var p in SerialPort.GetPortNames()) {
+                portName = p;
+                MenuItem m = new MenuItem();
+                m.Header = p;
+                m.Click += Com_Click;
+                COM.Items.Add(m);
+            }
+            if (portName != null && portName != "") {
+                COM.Header = String.Format("{0}(_C)", portName);
+                openPort(portName);
+            }
+        }
+
+        // IME変換中の判定は、こちらのblogを参考にさせて頂きました。
+        // https://yone64.wordpress.com/2010/10/25/ime%E3%81%A7%E5%A4%89%E6%8F%9B%E7%8A%B6%E6%85%8B%E4%B8%AD%E3%81%A7%E3%82%82textbox-textchanged%E3%81%8C%E7%99%BA%E7%94%9F%E3%81%99%E3%82%8B/
+        private void TB_TextChanged(object sender, TextChangedEventArgs e) {
+            if (_imeFlag) return;
+            //IMEで確定した場合のみ、ここに入る
+            string kanji = TB.Text;
+            if (kanji != "") {
+                // 文字が入力されていたら、送信する
+                Task.Run(() => sendKanji(kanji));
+            }
+            TB.Clear();
+            Console.WriteLine(kanji);
+        }
+
+        private void OnPreviewTextInput(object sender, TextCompositionEventArgs e) {
+            _imeFlag = false;
+        }
+
+        private void OnPreviewTextInputStart(object sender, TextCompositionEventArgs e) {
+            _imeFlag = true;
+        }
+
+        private void OnPreviewTextInputUpdate(object sender, TextCompositionEventArgs e) {
+            if (e.TextComposition.CompositionText.Length == 0)
+                _imeFlag = false;
+        }
+
+        private void Com_Click(object sender, RoutedEventArgs e) {
+            MenuItem menuitem = (MenuItem)sender; // オブジェクトをMenuItemクラスのインスタンスにキャストする。
+            string header = menuitem.Header.ToString(); // Headerプロパティを取り出して、文字列に変換する。
+            string tag = menuitem.Tag.ToString(); // Tagプロパティを取り出して、文字列に変換する。
+            Console.Out.WriteLine(String.Format("{0},{1}", header, tag));
+            COM.Header = String.Format("{0}(_C)", header);
+            openPort(header);
+        }
+
+        /// <summary>
+        /// COMポートのオープン
+        /// </summary>
+        private void openPort(String port) {
+            String PortName = port;
+            int BaudRate = 921600;
+            Parity Parity = Parity.None;
+            int DataBits = 8;
+            StopBits StopBits = StopBits.One;
+
+            myPort = new SerialPort(PortName, BaudRate, Parity, DataBits, StopBits);
+            myPort.Handshake = Handshake.XOnXOff;
+            myPort.Open();
+            myPort.DataReceived += new SerialDataReceivedEventHandler(Receive);
+            Console.Out.WriteLine(String.Format("Open [{0}].", PortName));
+        }
+
+        /// <summary>
+        /// COMポートからの受信
+        /// </summary>
+        private void Receive(object sender, System.IO.Ports.SerialDataReceivedEventArgs e) {
+            string dat = myPort.ReadExisting();
+            //Console.Out.WriteLine(String.Format("[{0}]", dat));
+        }
+
+        /// <summary>
+        /// キー入力
+        /// </summary>
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e) {
+            Key key = e.Key;
+            Key systemKey = e.SystemKey;
+            KeyStates keyStates = e.KeyStates;
+            bool isRepeat = e.IsRepeat;
+
+            // IMEの処理中でなければ、入力されたキーを送信する。
+            if (key != Key.ImeProcessed) {
+                if (key==Key.System) {
+                    key = systemKey;
+                }
+                sendKey(key);
+                e.Handled = true;
+            }
+
+            String s = String.Format("  Key=[{0}]\t  KeyStates=[{1}]\t  IsRepeat=[{2}]\t", key, keyStates, isRepeat);
+            ModifierKeys modifierKeys = Keyboard.Modifiers;
+            if ((modifierKeys & ModifierKeys.Alt) != ModifierKeys.None)
+                s += "  Alt ";
+            if ((modifierKeys & ModifierKeys.Control) != ModifierKeys.None)
+                s += "  Control ";
+            if ((modifierKeys & ModifierKeys.Shift) != ModifierKeys.None)
+                s += "  Shift ";
+            if ((modifierKeys & ModifierKeys.Windows) != ModifierKeys.None)
+                s += "  Windows";
+            if (key == Key.System)
+                s += systemKey;
+
+            s += String.Format(" Len=[{0}] T=[{1}]", TB.GetLineLength(0), TB.GetLineText(0));
+            //Console.Out.WriteLine(s);
+        }
+
+        /// <summary>
+        /// 漢字を送信する
+        /// </summary>
+        private void sendKanji(string kanji) {
+            // F11を送信してスマイルツールを起動
+            sendKey(0, KeyCode.getCode(Key.F11));
+            //Console.Out.WriteLine(String.Format("Send F11[{0}]",kanji));
+            Thread.Sleep(50);
+            int wait = 0;
+
+            foreach (char ch in kanji) {
+                UInt16 c = Convert.ToUInt16(ch);
+                //Console.Out.WriteLine(String.Format("[{0}]", Convert.ToString(c, 16)));
+
+                // UTF16コードを、上位から1ニブルずつ16進数の文字(0～F)に変換して送信
+                for (int i = 0; i < 4; i++) {
+                    string s = Convert.ToString((c>>12) & 0xf, 16);
+                    UInt16 fig = Convert.ToUInt16(s[0]);
+                    sendKey(0, (byte)KeyCode.asciiToScanCode[fig - 0x20]);
+                    Thread.Sleep(1);
+                    c <<= 4;
+                }
+                wait += 30;
+            }
+
+            // 最後に、終了を示すスペースを送信。この時点でスマイルツールが終了する。
+            sendKey(0, KeyCode.getCode(Key.Space));
+            Thread.Sleep(100+wait);
+
+            // Ctrl+Vを送信して貼り付け
+            sendKey(KeyCode.MOD_LCTRL, KeyCode.getCode(Key.V));
+        }
+
+        /// <summary>
+        /// キーを指定して送信
+        /// </summary>
+        private void sendKey(Key key) {
+            byte code = KeyCode.getCode(key);
+            ModifierKeys modKey = Keyboard.Modifiers;
+            byte mod = 0;
+            if ((modKey & ModifierKeys.Alt) != ModifierKeys.None) {
+                mod |= KeyCode.MOD_LALT;
+            }
+            if ((modKey & ModifierKeys.Control) != ModifierKeys.None) {
+                mod |= KeyCode.MOD_LCTRL;
+            }
+            if ((modKey & ModifierKeys.Shift) != ModifierKeys.None) {
+                mod |= KeyCode.MOD_LSHIFT;
+            }
+            if ((modKey & ModifierKeys.Windows) != ModifierKeys.None) {
+                mod |= KeyCode.MOD_LWINDOWS;
+            }
+            sendKey(mod, code);
+            //Console.Out.WriteLine(String.Format("Down[{0}] Send[{1}]", key, Convert.ToString(code, 16)));
+        }
+
+        /// <summary>
+        /// モディファイアとスキャンコードを指定して送信
+        /// </summary>
+        private void sendKey(byte mod, byte code) {
+            if (myPort != null) {
+                buf[0] = 0;
+                buf[1] = mod;
+                buf[2] = code;
+                myPort.Write(buf, 0, 3);
+            }
+        }
+
+        private void menuitemExit_Click(object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+    }
+}
